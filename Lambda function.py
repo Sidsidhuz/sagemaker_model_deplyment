3d71284d5d4e4b42ae25112d06eb06b99412aabe
SIDHARTH <98815208+Sidsidhuz@users.noreply.github.com>
2026-04-12 14:41:32 +0530

    2

---
+++ Lambda function.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+
+runtime = boto3.client("sagemaker-runtime")
+sns = boto3.client("sns")
+
+ENDPOINT_NAME = "pytorch-inference-2026-04-12-08-15-35-794"
+SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:260317865695:PlantDiseaseAlerts:f46c389a-f19f-45f2-95fd-0bc741bd461e"
+
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event["body"])
+        crop = body["crop"]
+
+        # SageMaker
+        response = runtime.invoke_endpoint(
+            EndpointName=ENDPOINT_NAME,
+            ContentType="application/json",
+            Body=event["body"]
+        )
+        result = json.loads(response["Body"].read().decode())
+        prediction = result["prediction"]
+
+
+        if prediction.lower() != "healthy": #SNS
+            message = f"Alert! {prediction} detected in your {crop} crop."
+            sns.publish(
+                TopicArn=SNS_TOPIC_ARN,
+                Message=message,
+                Subject="Plant Disease Detection Alert"
+            )
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"Predicted Disease": prediction, "Crop": crop})
+        }
+    except Exception as e:
+        return {"statusCode": 500, "body": str(e)}
